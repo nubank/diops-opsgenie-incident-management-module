5a15badf074d10b5eef2ac8b172a15ffd3858de6
Diego Rabatone Oliveira <diego.rabatone@nubank.com.br>
2023-12-13 11:09:31 -0300

    Improve notification policy resource/module
    Transform auto_close_action to be optional instead of mandatory.
    
    Adds the following blocks for notification policy:
    - de_duplication_action
    - delay_action
    - time_restriction

--- modules/config/notification_policies.tf
+++ modules/config/notification_policies.tf
@@ -25,10 +25,74 @@ resource "opsgenie_notification_policy" "this" {
     }
   }
 
-  auto_close_action {
-    duration {
-      time_amount = try(each.value.auto_close_action.time_amount, null)
-      time_unit   = try(each.value.auto_close_action.time_unit, "minutes")
+  time_restriction {
+    type = try(local.notification_policy.time_restriction.type, null)
+
+    dynamic "restrictions" {
+      for_each = try(local.notification_policy.filter.conditions, [])
+
+      content {
+        start_day  = try(conditions.value.start_day, null)
+        end_day    = try(conditions.value.end_day, null)
+        start_hour = try(conditions.value.start_hour, null)
+        end_hour   = try(conditions.value.end_hour, null)
+        start_min  = try(conditions.value.start_min, null)
+        end_min    = try(conditions.value.end_min, null)
+      }
     }
   }
+
+  dynamic "de_duplication_action" {
+    for_each = try(local.notification_policy.de_duplication_action, null) != null ? ["true"] : []
+
+    content {
+      de_duplication_action_type = local.notification_policy.de_duplication_action.de_duplication_action_type
+      count                      = local.notification_policy.de_duplication_action.count
+
+      dynamic "duration" {
+        for_each = local.notification_policy.de_duplication_action.de_duplication_action_type == "frequency-based" ? ["true"] : [try(local.notification_policy.de_duplication_action.duration, null)]
+
+        content {
+          time_amount = local.notification_policy.de_duplication_action.duration.time_amount
+          time_unit   = try(local.notification_policy.de_duplication_action.duration.time_unit, null)
+        }
+      }
+
+      until_hour   = local.notification_policy.delay_action.delay_option != "for-duration" ? local.notification_policy.delay_action.until_hour : null
+      until_minute = local.notification_policy.delay_action.delay_option != "for-duration" ? local.notification_policy.delay_action.until_minute : null
+    }
+  }
+
+  dynamic "delay_action" {
+    for_each = try(local.notification_policy.delay_action, null) != null ? ["true"] : []
+
+    content {
+      delay_option = local.notification_policy.delay_action.delay_option
+
+      dynamic "duration" {
+        for_each = local.notification_policy.delay_action.delay_option == "for-duration" ? ["true"] : []
+
+        content {
+          time_amount = local.notification_policy.delay_action.duration.time_amount
+          time_unit   = try(local.notification_policy.delay_action.duration.time_unit, null)
+        }
+      }
+
+      until_hour   = local.notification_policy.delay_action.delay_option != "for-duration" ? local.notification_policy.delay_action.until_hour : null
+      until_minute = local.notification_policy.delay_action.delay_option != "for-duration" ? local.notification_policy.delay_action.until_minute : null
+    }
+  }
+
+  dynamic "auto_close_action" {
+    for_each = try(local.notification_policy.auto_close_action, null) != null ? ["true"] : []
+
+    content {
+      duration {
+        time_amount = local.notification_policy.auto_close_action.duration.time_amount
+        time_unit   = local.notification_policy.auto_close_action.duration.time_unit
+      }
+    }
+
+  }
+
 }
