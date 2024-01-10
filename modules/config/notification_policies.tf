65c768453ea5b153df0541afadaab4c92ef9841c
Diego Rabatone Oliveira <diego.rabatone@nubank.com.br>
2024-01-10 17:51:38 -0300

    Refactoring notifications_policy on config module

--- modules/config/notification_policies.tf
+++ modules/config/notification_policies.tf
@@ -2,7 +2,7 @@ resource "opsgenie_notification_policy" "this" {
   for_each = module.this.enabled ? { for policy in local.notification_policies : policy.name => policy } : tomap()
 
   enabled = try(each.value.enabled, true)
-  name    = each.value.name
+  name    = each.key
 
   # Look up our team id by name
   team_id            = opsgenie_team.this[each.value.team_name].id
@@ -15,84 +15,110 @@ resource "opsgenie_notification_policy" "this" {
       for_each = try(each.value.filter.conditions, [])
 
       content {
-        expected_value = try(conditions.value.expected_value, null)
-        field          = try(conditions.value.field, null)
+        field          = conditions.value.field
+        operation      = conditions.value.operation
+        expected_value = try(conditions.value.expected_value, "")
         key            = try(conditions.value.key, null)
-        not            = try(conditions.value.not, null)
-        operation      = try(conditions.value.operation, null)
+        not            = try(conditions.value.not, false)
         order          = try(conditions.value.order, null)
       }
     }
   }
 
-  time_restriction {
-    type = try(local.notification_policy.time_restriction.type, null)
-
-    dynamic "restrictions" {
-      for_each = try(local.notification_policy.filter.conditions, [])
-
-      content {
-        start_day  = try(conditions.value.start_day, null)
-        end_day    = try(conditions.value.end_day, null)
-        start_hour = try(conditions.value.start_hour, null)
-        end_hour   = try(conditions.value.end_hour, null)
-        start_min  = try(conditions.value.start_min, null)
-        end_min    = try(conditions.value.end_min, null)
-      }
-    }
-  }
-
-  dynamic "de_duplication_action" {
-    for_each = try(local.notification_policy.de_duplication_action, null) != null ? ["true"] : []
+  dynamic "time_restriction" {
+    for_each = try(each.value.time_restriction, null) != null ? [each.value.time_restriction] : []
 
     content {
-      de_duplication_action_type = local.notification_policy.de_duplication_action.de_duplication_action_type
-      count                      = local.notification_policy.de_duplication_action.count
+      type = time_restriction.value.type
 
-      dynamic "duration" {
-        for_each = local.notification_policy.de_duplication_action.de_duplication_action_type == "frequency-based" ? ["true"] : [try(local.notification_policy.de_duplication_action.duration, null)]
+      dynamic "restrictions" {
+        for_each = try(time_restriction.value.restrictions, null) != null ? [time_restriction.value.restrictions] : []
 
         content {
-          time_amount = local.notification_policy.de_duplication_action.duration.time_amount
-          time_unit   = try(local.notification_policy.de_duplication_action.duration.time_unit, null)
+          start_day  = restrictions.value.start_day
+          end_day    = restrictions.value.end_day
+          start_hour = restrictions.value.start_hour
+          end_hour   = restrictions.value.end_hour
+          start_min  = restrictions.value.start_min
+          end_min    = restrictions.value.end_min
         }
       }
 
-      until_hour   = local.notification_policy.delay_action.delay_option != "for-duration" ? local.notification_policy.delay_action.until_hour : null
-      until_minute = local.notification_policy.delay_action.delay_option != "for-duration" ? local.notification_policy.delay_action.until_minute : null
-    }
-  }
-
-  dynamic "delay_action" {
-    for_each = try(local.notification_policy.delay_action, null) != null ? ["true"] : []
-
-    content {
-      delay_option = local.notification_policy.delay_action.delay_option
-
-      dynamic "duration" {
-        for_each = local.notification_policy.delay_action.delay_option == "for-duration" ? ["true"] : []
+      dynamic "restriction" {
+        for_each = try(time_restriction.value.restriction, null) != null ? [time_restriction.value.restriction] : []
 
         content {
-          time_amount = local.notification_policy.delay_action.duration.time_amount
-          time_unit   = try(local.notification_policy.delay_action.duration.time_unit, null)
+          start_hour = restriction.value.start_hour
+          end_hour   = restriction.value.end_hour
+          start_min  = restriction.value.start_min
+          end_min    = restriction.value.end_min
         }
       }
-
-      until_hour   = local.notification_policy.delay_action.delay_option != "for-duration" ? local.notification_policy.delay_action.until_hour : null
-      until_minute = local.notification_policy.delay_action.delay_option != "for-duration" ? local.notification_policy.delay_action.until_minute : null
     }
   }
 
   dynamic "auto_close_action" {
-    for_each = try(local.notification_policy.auto_close_action, null) != null ? ["true"] : []
+    for_each = try(each.value.auto_close_action, null) != null ? [each.value.auto_close_action] : []
 
     content {
       duration {
-        time_amount = local.notification_policy.auto_close_action.duration.time_amount
-        time_unit   = local.notification_policy.auto_close_action.duration.time_unit
+        time_amount = auto_close_action.value.duration.time_amount
+        time_unit   = try(auto_close_action.value.duration.time_unit, "minutes")
       }
     }
 
   }
 
+  dynamic "auto_restart_action" {
+    for_each = try(each.value.auto_restart_action, null) != null ? [each.value.auto_restart_action] : []
+
+    content {
+      duration {
+        time_amount = auto_restart_action.value.duration.time_amount
+        time_unit   = try(auto_restart_action.value.duration.time_unit, "minutes")
+      }
+      max_repeat_count = auto_restart_action.value.max_repeat_count
+    }
+
+  }
+
+  dynamic "de_duplication_action" {
+    for_each = try(each.value.de_duplication_action, null) != null ? [each.value.de_duplication_action] : []
+
+    content {
+      de_duplication_action_type = de_duplication_action.value.de_duplication_action_type
+      count                      = de_duplication_action.value.count
+
+      dynamic "duration" {
+        for_each = each.value.de_duplication_action.de_duplication_action_type == "frequency-based" ? [each.value.de_duplication_action.duration] : [try(each.value.de_duplication_action.duration, null)]
+
+        content {
+          time_amount = duration.value.time_amount
+          time_unit   = try(duration.value.time_unit, "minutes")
+        }
+      }
+    }
+  }
+
+  dynamic "delay_action" {
+    for_each = try(each.value.delay_action, null) != null ? [each.value.delay_action] : []
+
+    content {
+      delay_option = delay_action.value.delay_option
+
+      dynamic "duration" {
+        for_each = delay_action.value.delay_option == "for-duration" ? [delay_action.value.duration] : []
+
+        content {
+          time_amount = duration.value.time_amount
+          time_unit   = try(duration.valeu.time_unit, "minutes")
+        }
+      }
+
+      until_hour   = delay_action.value.delay_option != "for-duration" ? delay_action.value.until_hour : null
+      until_minute = delay_action.value.delay_option != "for-duration" ? delay_action.value.until_minute : null
+    }
+  }
+
+  suppress = try(each.value.suppress, null)
 }
